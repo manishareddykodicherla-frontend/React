d8a3d837a6430d68167ecefd5dcccca49eb39288
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-04-24 10:44:29 -0700

    test commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './Components/Nav';
+import List from './Components/List';
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/List.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function List() {
+
+  return (
+    <div>
+         <div className="movie__lists">
+        <div  id='movies'className="movie__list">
+    <figure className="movieImage">
+        <img src="" className="movielogo"/>
+    </figure>
+    <h2  id= '#tickets' className="moviePrice">MoviePrice</h2>
+    <h4 className="name">MovieName</h4>
+    <h4 className="Year">Year</h4>
+    <h4 class="imdb">IMDB</h4>
+   </div>
+        </div>
+    </div>
+  )
+}
