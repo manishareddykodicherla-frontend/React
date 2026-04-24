55ec7cc76042d6253b1d488faff9a34a506246d6
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-04-24 11:32:53 -0700

    added

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState, useEf } from 'react'
 
 export default function List() {
-
+const movies=`https://www.omdbapi.com/?i=tt3896198&apikey=9a6c4194&s=movie`
+const [data, setData] = useState([]);
+useEffect(()=>{
+    const fetchData=async()=>{
+    const response= await fetch(movies);
+    const data=await response.json();
+    setData(data.Search);
+    }
+    fetchData();
+    console.log(data);
+},[]
+)
   return (
     <div>
          <div className="movie__lists">
         <div  id='movies'className="movie__list">
     <figure className="movieImage">
-        <img src="" className="movielogo"/>
+        <img src={data.Poster} className="movielogo"/>
     </figure>
     <h2  id= '#tickets' className="moviePrice">MoviePrice</h2>
     <h4 className="name">MovieName</h4>
     <h4 className="Year">Year</h4>
-    <h4 class="imdb">IMDB</h4>
+    <h4 className="imdb">IMDB</h4>
    </div>
         </div>
     </div>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import image from './assets/1000_F_500611919_5wuf1qGRCubiXXxIa7og1fLLCyHi6qP9.jpg'
+import image from '../assets/1000_F_500611919_5wuf1qGRCubiXXxIa7og1fLLCyHi6qP9.jpg'
 export default function Nav() {
+
+  
   return (
     <div>
     <nav>
-        <figure class="movie">
-            <img src={image} alt=""class="navlogo"/>
+        <figure className="movie">
+            <img src={image} alt=""className="navlogo"/>
         </figure>
         <ul class="nav__lists">
             <li className="nav__list"><a href="">Home</a></li>
